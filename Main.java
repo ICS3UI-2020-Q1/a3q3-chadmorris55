64f1444a0fd4776dbb02d6724b5104e63d5deaa7
chadmorris55 <71134162+chadmorris55@users.noreply.github.com>
2020-10-01 12:13:12 -0400

    Update Main.java
    tells user what percentage they got based off of their letter grade

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * finds the largest integer out of the three given
+ * @Chad 
  */
 public class Main {
 
@@ -9,7 +10,26 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // creates Scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //asks user for the letter grade they got
+    System.out.println("Please enter a letter grade");
+    String grade = input.nextLine();
+    switch (grade){
+      case "A":
+        System.out.println("This is between 80% and 100%");
+        break;
+      case "B":
+        System.out.println("This is between 70% and 79%");
+        break;
+      case "C":
+        System.out.println("This is between 60% and 69%");
+      case "D":
+        System.out.println("This is between 50% and 59%");
+      case "F": 
+        System.out.println("This is between 0% and 49%");
+    }
   }
 }
+
